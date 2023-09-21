ff4c48a3832734309482ba9d71f28b4caaaf9070
Vitor Cesario <vitorsousa676@gmail.com>
2023-09-21 18:35:23 -0300

    Hello World

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace helloworld.Controllers
+{
+    [Route("~/")]
+    [ApiController]
+    public class HelloController
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "Hello World !!";
+        }
+
+
+        [HttpGet("/bsm")]
+        public string Bsms()
+        {
+            return "Responsabilidade Pessoal, Mentalidade de Crescimento, Orientação ao Futuro, Persistência, Comunicação, Orientação aos Detalhes, Trabalho em Equipe, Proatividade";
+                
+            
+               
+        }
+        [HttpGet("/objetivosdeaprendizagem")]
+        public string ObjetivosdeAprendizagem()
+        {
+            return "Melhor entendimento no conteúdo prático ";
+
+
+
+        }
+
+    }
+}
